5d49ff2d3b9bc824013a43ae4c93d2b2b972c71c
wicros824 <wicroswts@gmail.com>
2019-03-26 19:13:51 +0900

    Add valid block chain

--- app/controllers/blockchain_controller.rb
+++ app/controllers/blockchain_controller.rb
@@ -11,4 +11,8 @@ class BlockchainController < ApplicationController
     redirect_to action: "index"
   end
 
+  def valid
+    render plain: Block.all.each_cons(2).all? { |a, b| a.current_hash == b.previous_hash }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "blocks",  to: "blockchain#index"
   get "blocks/new",  to: "blockchain#new"
+  get "blockchain/new",  to: "blockchain#valid"
 
   get "transactions",  to: "transactions#index"
   post "transactions", to: "transactions#create"
